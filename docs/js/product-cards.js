9bd74fb9e3e6ee7a52b24512213475c6517cea30
ADudin <gwaeron27@yandex.ru>
2021-06-16 11:13:03 +0300

    Вынес отрисовку карточки товара в скрипте в функцию, доавил отрисованной карточке приоритет в стилях, поправил размеры социальных ссылок в футере

--- docs/js/product-cards.js
+++ docs/js/product-cards.js
@@ -1,5 +1,3 @@
-const cardsList = document.querySelectorAll('.catalog__list-item');
-
 const PRODUCT_DESCRIPTIONS = [
   'Odio ut sem nulla pharetra diam sit amet. Risus viverra adipiscing at in.',
   'Gravida neque convallis a cras. Ut consequat semper viverra nam libero justo laoreet sit. Nunc mattis enim ut tellus elementum sagittis vitae et leo.',
@@ -12,56 +10,65 @@ const PRODUCT_DESCRIPTIONS = [
   'Turpis tincidunt id aliquet risus feugiat. Dui sapien eget mi proin sed libero. Pretium viverra suspendisse potenti nullam ac tortor.',
 ];
 
-for(let i = 0; i < cardsList.length; i++) {
+const main = document.querySelector('.page-main');
+const cardsList = main.querySelectorAll('.catalog__list-item');
+const catalog = main.querySelector('.catalog');
+const template = main.querySelector('#card').content;
+const subscription = main.querySelector('.subscription');
+
+const renderProductCard = (productItem) => {
+  const imageSource = productItem.querySelector('.catalog__list-item__img').getAttribute('src');
+  const title = productItem.querySelector('.catalog__list-item__description').textContent;
+  const price = productItem.querySelector('.catalog__list-item__price').innerHTML;
+
+  const productCard = template.cloneNode(true);
+  const productImage = productCard.querySelector('.product-card__img');
+  const productImageContainer = productCard.querySelector('.product-card__img-container');
+  const productTitle = productCard.querySelector('.product-card__title');
+  const productPrice = productCard.querySelector('.product-card__price');
+  const closeButton = productCard.querySelector('.product-card__close-button');
+  const productDescription = productCard.querySelector('.product-card__description');
+
+  productImage.src = imageSource;
+  productTitle.textContent = title;
+  productDescription.textContent = PRODUCT_DESCRIPTIONS[i];
+  productPrice.innerHTML = price;
+
+  if (productItem.classList.contains('catalog__list-item--new')) {
+    productImageContainer.classList.add('product-card__img-container--new');
+  }
+
+  if (productItem.classList.contains('catalog__list-item--sale')) {
+    productImageContainer.classList.add('product-card__img-container--sale');
+  }
+
+  catalog.classList.add('muted');
+  subscription.classList.add('muted');
+  main.appendChild(productCard);
+
+  const closeProductCardHandler = () => {
+    const popup = main.querySelector('.product-card');
+    main.removeChild(popup);
+    catalog.classList.remove('muted');
+    subscription.classList.remove('muted');
+    closeButton.removeEventListener('click', closeProductCardHandler);
+    document.removeEventListener('keydown', onEscKeydown);
+  }
+
+  const onEscKeydown = (evt) => {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      closeProductCardHandler();
+    }
+  }
+
+  document.addEventListener('keydown', onEscKeydown);
+  closeButton.addEventListener('click', closeProductCardHandler);
+}
+
+for (let i = 0; i < cardsList.length; i++) {
   cardsList[i].addEventListener('click', (evt) => {
     evt.preventDefault();
-    const imageSource = cardsList[i].querySelector('.catalog__list-item__img').getAttribute('src');
-    const title = cardsList[i].querySelector('.catalog__list-item__description').textContent;
-    const price = cardsList[i].querySelector('.catalog__list-item__price').innerHTML;
-
-    const template = document.querySelector('#card').content;
-    const productCard = template.cloneNode(true);
-    const productImage = productCard.querySelector('.product-card__img');
-    const productImageContainer = productCard.querySelector('.product-card__img-container');
-    const productTitle = productCard.querySelector('.product-card__title');
-    const productPrice = productCard.querySelector('.product-card__price');
-    const closeButton = productCard.querySelector('.product-card__close-button');
-    const productDescription = productCard.querySelector('.product-card__description');
-
-    productImage.src = imageSource;
-    productTitle.textContent = title;
-    productDescription.textContent = PRODUCT_DESCRIPTIONS[i];
-    productPrice.innerHTML = price;
-
-    if (cardsList[i].classList.contains('catalog__list-item--new')) {
-      productImageContainer.classList.add('product-card__img-container--new');
-    }
-
-    if (cardsList[i].classList.contains('catalog__list-item--sale')) {
-      productImageContainer.classList.add('product-card__img-container--sale');
-    }
-
-    const main = document.querySelector('.page-main');
-    const catalog = main.querySelector('.catalog');
-    catalog.classList.add('muted');
-    main.appendChild(productCard);
-
-    const closeProductCardHandler = () => {
-      const popup = main.querySelector('.product-card');
-      main.removeChild(popup);
-      catalog.classList.remove('muted');
-      closeButton.removeEventListener('click', closeProductCardHandler);
-      document.removeEventListener('keydown', onEscKeydown);
-    }
-
-    const onEscKeydown = (evt) => {
-      if (evt.keyCode === 27) {
-        evt.preventDefault();
-        closeProductCardHandler();
-      }
-    }
-
-    document.addEventListener('keydown', onEscKeydown);
-    closeButton.addEventListener('click', closeProductCardHandler);
+    renderProductCard(cardsList[i]);
   })
-};
+}
